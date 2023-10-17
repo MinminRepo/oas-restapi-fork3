55545fe432990bbd74ea6b0e1094d0090c8e2b51
MinminRepo <imthepapercat@gmail.com>
2023-10-17 13:09:51 +0800

    Added some documentation on the authentication mechanism (comments).

--- typescript/src/routes/account.login.ts
+++ typescript/src/routes/account.login.ts
@@ -3,6 +3,25 @@ const crypt = require('../../build/modules/cryptography');
 const db = require('../../build/modules/database');
 
 module.exports.accountLogin = async (req: any, res: any) => {
+    /**
+     * Documentation:
+     * 
+     * Requirements: 
+     * username as string, password as pre-hashed string in SHA256.
+     * 
+     * Process: 
+     * 1. The user supplies the username and pre-hashed password from the front-end.
+     * 2. The API will salt and hash the password.
+     * 3. The API will query the database using the username and the output from item #2.
+     * 4. The API will check for the length of the resulting query. If the length is 1,
+     * then an account was found. 
+     * 5. If the length is 1, issue a JSON Web Token (JWT) derived from the resulting object from
+     * the query.
+     * 6. Send the JWT together with a status code of 200 to the client.
+     * 7. On system errors, send a 500 status code.
+     * 8. Send a 401 status code if the result from the query has a length of 0.
+     * 
+     */
 
     try {
 
